98fab6ab567f2547f64be69f863aa78256f9a193
chris <chrislawrence05@gmail.com>
2018-11-24 16:30:43 -0500

    Adds relationships via FluentApi

--- Domain/HomeDbContext.cs
+++ Domain/HomeDbContext.cs
@@ -7,7 +7,24 @@ namespace Home.Api
         public HomeDbContext(DbContextOptions<HomeDbContext> options) : base(options)
         { }
 
-        //TODO: Add relationships via Fluent API??
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Room>()
+                .HasOne(p => p.Home)
+                .WithMany(p => p.Rooms);
+
+            modelBuilder.Entity<Window>()
+                .HasOne(p => p.Room)
+                .WithMany(p => p.Windows);
+
+            modelBuilder.Entity<LightBulb>()
+                .HasOne(p => p.Room)
+                .WithMany(p => p.LightBulbs);
+
+            modelBuilder.Entity<Door>()
+                .HasOne(p => p.Room)
+                .WithMany(p => p.Doors);
+        }
 
         public DbSet<Home> Homes { get; set; }
         public DbSet<Room> Rooms { get; set; }
