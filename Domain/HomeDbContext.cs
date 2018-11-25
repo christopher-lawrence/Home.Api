57bf0c0fa18c8e038fb719cd63f6d6f8adcd6b17
chris <chrislawrence05@gmail.com>
2018-11-24 23:06:35 -0500

    Initial Db requirements

--- Domain/HomeDbContext.cs
+++ Domain/HomeDbContext.cs
@@ -12,22 +12,50 @@ namespace Home.Api
             modelBuilder.Entity<Room>()
                 .HasOne(p => p.Home)
                 .WithMany(p => p.Rooms);
+            modelBuilder.Entity<Room>()
+                .Property(p => p.HomeId)
+                .IsRequired();
+
+            modelBuilder.Entity<Room>()
+                .HasMany(p => p.Colors)
+                .WithOne();
 
             modelBuilder.Entity<Window>()
                 .HasOne(p => p.Room)
                 .WithMany(p => p.Windows);
+            modelBuilder.Entity<Window>()
+                .Property(p => p.RoomId)
+                .IsRequired();
+            modelBuilder.Entity<Window>()
+                .HasOne(p => p.Color)
+                .WithOne();
 
             modelBuilder.Entity<LightBulb>()
                 .HasOne(p => p.Room)
                 .WithMany(p => p.LightBulbs);
+            modelBuilder.Entity<LightBulb>()
+                .Property(p => p.RoomId)
+                .IsRequired();
 
             modelBuilder.Entity<Door>()
                 .HasOne(p => p.Room)
                 .WithMany(p => p.Doors);
+            modelBuilder.Entity<Door>()
+                .Property(p => p.RoomId)
+                .IsRequired();
+            modelBuilder.Entity<Door>()
+                .HasOne(p => p.Color)
+                .WithOne();
 
             modelBuilder.Entity<Floor>()
                 .HasOne(p => p.Room)
                 .WithOne(p => p.Floor);
+            modelBuilder.Entity<Floor>()
+                .Property(p => p.RoomId)
+                .IsRequired();
+            modelBuilder.Entity<Floor>()
+                .HasOne(p => p.Color)
+                .WithOne();
         }
 
         public DbSet<Home> Homes { get; set; }
@@ -36,5 +64,6 @@ namespace Home.Api
         public DbSet<LightBulb> LightBulbs { get; set; }
         public DbSet<Door> Doors { get; set; }
         public DbSet<Floor> Floors { get; set; }
+        public DbSet<Color> Colors { get; set; }
     }
 }
