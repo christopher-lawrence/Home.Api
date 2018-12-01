ded4c9c8bb61caf27e96ea843b792b4713b9a8a5
chris <chrislawrence05@gmail.com>
2018-12-01 06:33:35 -0500

    Adds Home get rooms handler

---
+++ Features/Home/GetRoomsHandler.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Home.Api.Domain;
+using Home.Api.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Home.Api.Features.Home
+{
+    public class GetRoomsRequest : IRequest<List<Room>>
+    {
+        public Guid HomeId { get; set; }
+    }
+
+    public class GetRoomsHandler : IRequestHandler<GetRoomsRequest, List<Room>>
+    {
+        private HomeDbContext _dbContext;
+
+        public GetRoomsHandler(HomeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<List<Room>> Handle(GetRoomsRequest request, CancellationToken cancellationToken)
+        {
+            var result = _dbContext.Rooms.Include(r => r.Floor).Include(r => r.Home).Where(r => r.HomeId == request.HomeId);
+            return result.ToListAsync();
+        }
+    }
+}
