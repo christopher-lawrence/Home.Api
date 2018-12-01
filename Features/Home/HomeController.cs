847943159f56a0f94e7fabe14fbd0491bdbc4fb5
chris <chrislawrence05@gmail.com>
2018-12-01 06:33:51 -0500

    Update HomeController with new endpoints

--- Features/Home/HomeController.cs
+++ Features/Home/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Home.Api.Models;
 using MediatR;
+using Home.Api.Domain;
 
 namespace Home.Api.Features.Home
 {
@@ -38,7 +39,7 @@ namespace Home.Api.Features.Home
         }
 
         [HttpGet]
-        [Route("get/{id}")]
+        [Route("{id}")]
         public Task<Models.Home> GetHomeAsync(Guid id)
         {
             return _mediator.Send(new GetHomeRequest { Id = id });
@@ -48,11 +49,35 @@ namespace Home.Api.Features.Home
         [Route("update/{id}")]
         public Task<Models.Home> UpdateHomeAsync(Guid id, [FromBody]Models.Home home)
         {
+            if (!ModelState.IsValid)
+            {
+                throw new Exception("bad state");
+            }
             if (id != home.Id)
             {
                 return null;
             }
             return _mediator.Send(new UpdateHomeRequest { Home = home });
         }
+
+        [HttpPost]
+        [Route("{id}/room/add")]
+        public Task<Room> AddRoomAsync(Guid id, [FromBody]Room room)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new Exception("bad state");
+            }
+            room.HomeId = id;
+            return _mediator.Send(new AddRoomRequest { Room = room });
+        }
+
+        [HttpGet]
+        [Route("{id}/rooms")]
+        public Task<List<Room>> GetRoomsAsync(Guid id)
+        {
+            return _mediator.Send(new GetRoomsRequest { HomeId = id });
+        }
+
     }
 }
